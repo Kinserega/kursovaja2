d4f436496edb0fe6310dc84d4a64261c931213fb
Kinserega <kindratsergej5@gmail.com>
2023-07-21 19:23:02 +0300

    курсовая 2

---
+++ src/test/java/pro/sky/kursovaja2/Kursovaja2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.kursovaja2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaja2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
